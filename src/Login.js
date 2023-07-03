64a32d4466c2f7372065e672ca25a4b0e9ea9b4a
ThuyTrang-IT <63693331+ThuyTrang-IT@users.noreply.github.com>
2023-07-03 22:40:31 +0700

    modified:   src/Login.js 	modified:   src/utils/HandleApi.js

--- src/Login.js
+++ src/Login.js
@@ -1,52 +1,66 @@
 import React, { useState } from 'react';
 import { login } from './utils/LoginApi';
+import { useHistory } from 'react-router-dom';
 import { Form, Button } from 'react-bootstrap';
-import { Redirect, useHistory } from 'react-router-dom';
 
 function LoginPage() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
-  const [isLoading, setIsLoading] = useState(false); // Thêm state isLoading
-  const [success, setSuccess] = useState(false); // Thêm state success
-  const [redirect, setRedirect] = useState(false); // Thêm state redirect
-  const history = useHistory(); // Hook để chuyển hướng đến trang to do sau khi đăng nhập thành công
+  const [isLoading, setIsLoading] = useState(false);
+  const [success, setSuccess] = useState(false);
+  const [redirect, setRedirect] = useState(false);
+  const history = useHistory();
 
   const handleLogin = async (e) => {
     e.preventDefault();
-    setIsLoading(true); // Bắt đầu đăng nhập và đang tải
+    setIsLoading(true);
     try {
       const data = await login(email, password);
-      localStorage.setItem('access_token', data.access_token);
-      setIsLoading(false); // Kết thúc đăng nhập và đang tải
-      setSuccess(true); // Đăng nhập thành công
-      setRedirect(true); // Thiết lập redirect thành true
+      if (data) { // Kiểm tra xem biến data có giá trị hay không
+        localStorage.setItem('access_token', data.access_token);
+        setIsLoading(false);
+        setSuccess(true);
+        setTimeout(() => {
+          setRedirect(true);
+        }, 2000);
+      }
     } catch (error) {
-      setIsLoading(false); // Kết thúc đăng nhập và đang tải
+      setIsLoading(false);
       setError(error.response.data.message);
     }
   };
+  
 
   return (
     <div className="row justify-content-center mt-5">
-      <div className="col-6">
+      <div className="col-md-6">
         <h2 className="text-center mb-4">Login</h2>
-        <Form onSubmit={handleLogin}>
-          {error && <div className="alert alert-danger">{error}</div>}
-          {success && <div className="alert alert-success">Login success. Redirecting to Todo page...</div>} // Hiển thị thông báo đăng nhập thành công và chuyển hướng
-          <Form.Group controlId="email">
+        <form onSubmit={handleLogin}>
+        
+          <Form.Group controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
             <Form.Control type="email" placeholder="Enter email" value={email} onChange={(e) => setEmail(e.target.value)} required />
           </Form.Group>
-          <Form.Group controlId="password">
+
+          <Form.Group controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
             <Form.Control type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} required />
           </Form.Group>
           <Button variant="primary" type="submit" disabled={isLoading}>
-            {isLoading ? 'Loading...' : 'Login'} // Hiển thị trạng thái đang tải khi đăng nhập
+            {isLoading ? 'Loading...' : 'Login'}
           </Button>
-        </Form>
-        {redirect && <Redirect to="/todo" />} // Sử dụng Redirect để chuyển hướng đến trang to do khi redirect là true
+        </form>
+        {isLoading && <div>Loading...</div>}
+        {error && <div className="alert alert-danger mt-3">{error}</div>}
+        {success && (
+          <div className="alert alert-success mt-3">
+            Login success. Redirecting to Todo page...
+          </div>
+          
+        )}
+      
+        {redirect && history.push('/todo')}
       </div>
     </div>
   );
--- src/utils/HandleApi.js
+++ src/utils/HandleApi.js
@@ -4,27 +4,27 @@ const API_ENDPOINT = 'http://localhost:3006';
 
 // Hàm để lấy tất cả các công việc
 export const getAllTasks = () => {
-  return axios.get(`${API_ENDPOINT}/todos`).then((response) => response.data);
+  return axios.get(`${API_ENDPOINT}/todo`).then((response) => response.data);
 };
 
 // Hàm để lấy một công việc theo ID
 export const getTaskById = (id) => {
   return axios
-    .get(`${API_ENDPOINT}/todos/${id}`)
+    .get(`${API_ENDPOINT}/todo/${id}`)
     .then((response) => response.data);
 };
 
 // Hàm để thêm một công việc mới
 export const addTask = (task) => {
-  return axios.post(`${API_ENDPOINT}/todos`, task).then((response) => response.data);
+  return axios.post(`${API_ENDPOINT}/todo`, task).then((response) => response.data);
 };
 
 // Hàm để cập nhật một công việc
 export const updateTask = (id, task) => {
-  return axios.put(`${API_ENDPOINT}/todos/${id}`, task).then((response) => response.data);
+  return axios.put(`${API_ENDPOINT}/todo/${id}`, task).then((response) => response.data);
 };
 
 //Hàm để xóa một công việc
 export const deleteTask = (id) => {
-  return axios.delete(`${API_ENDPOINT}/todos/${id}`).then((response) => response.data);
+  return axios.delete(`${API_ENDPOINT}/todo/${id}`).then((response) => response.data);
 };
