1be4bf3537495e180db16a9269c23706fa38b638
ThuyTrang-IT <63693331+ThuyTrang-IT@users.noreply.github.com>
2023-07-19 16:30:19 +0700

    Update LoginApi.js

--- src/utils/LoginApi.js
+++ src/utils/LoginApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:3006'; // Đường dẫn đến server API
+const BASE_URL = 'http://trangpham.gcalls.vn/api'; // Đường dẫn đến server API
 
 // Hàm gửi yêu cầu đăng nhập và trả về thông tin người dùng và mã thông báo truy cập
 export const login = async (email, password) => {
