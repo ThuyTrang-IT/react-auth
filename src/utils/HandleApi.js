62959b650a6bf5787f9bd1ab707adc31934cce99
ThuyTrang-IT <63693331+ThuyTrang-IT@users.noreply.github.com>
2023-07-19 16:30:32 +0700

    Update HandleApi.js

--- src/utils/HandleApi.js
+++ src/utils/HandleApi.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const API_ENDPOINT = 'http:trangpham.gcalls.vn/api';
+const API_ENDPOINT = 'http://trangpham.gcalls.vn/api';
 
 // Hàm để lấy tất cả các công việc
 export const getAllTasks = () => {
